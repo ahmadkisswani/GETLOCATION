cb6cf579867fa10ccd69f7eed9146432e780d632
ahmadkisswani <67906076+ahmadkisswani@users.noreply.github.com>
2023-10-04 12:33:34 +0000

    first  committed

--- js/coreApi.js
+++ js/coreApi.js
@@ -1,30 +1,29 @@
-
 window.addEventListener('DOMContentLoaded', (event) => {
-    
-    $.get( "https://json.geoiplookup.io", function( data ) {
+    $.get("https://json.geoiplookup.io", function(data) {
+        var cont = document.getElementsByClassName('w3-container')[0];
 
-var  cont=  document.getElementsByClassName('w3-container')[2];
-    for (const key in data) {
-        var x = document.createElement('li');
-        x.setAttribute('style', 'height: 40px')
+        // Create a ul element with Bootstrap list-group class
+        var ul = document.createElement('ul');
+        ul.className = 'list-group list-group-flush';
 
-        var y =  document.createElement('hr');
-        //border-width:0
-        y.setAttribute('style','border-width:2 ; ');
-      
-    
-        x.innerHTML =key +": &nbsp "+ data[key];
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                // Create an li element with Bootstrap list-group-item class
+                var x = document.createElement('li');
+                x.className = 'list-group-item';
 
-        cont.appendChild(x); 
-        cont.appendChild(y);
-    }
+                var y = document.createElement('hr');
+                y.style.border = 'none';
 
+                x.innerHTML = key + ": &nbsp " + data[key];
+
+                // Append the li element to the ul
+                ul.appendChild(x);
+                ul.appendChild(y);
+            }
+        }
+
+        // Append the ul element to the container
+        cont.appendChild(ul);
     });
-})
-
- 
-
-
-
-
-
+});
